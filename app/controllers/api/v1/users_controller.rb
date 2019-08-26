060f7aed30d04fb49dd64e634e50c535d0c5c30e
Vsevolod Voloshyn <seva.voloshin@gmail.com>
2019-08-26 14:21:56 +0300

    Moving to api/v1 namespace

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
   before_action :authorize_request, except: :create
   before_action :find_user, except: %i[create index]
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
   post '/auth/login', to: 'authentication#login'
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :users
+    end
+  end
 end
