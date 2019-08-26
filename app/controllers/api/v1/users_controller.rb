37b03738213172a123c9eec2f1732998209cf240
Vsevolod Voloshyn <seva.voloshin@gmail.com>
2019-08-26 16:44:47 +0300

    Using local variable instead unnecessary instance variable

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,11 +7,11 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user, status: :created
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
     else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
